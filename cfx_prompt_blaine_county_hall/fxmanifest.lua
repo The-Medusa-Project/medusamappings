786a62e0c3146a84916760fdf81a095f30db4323
celenys <ripertl@gmail.com>
2026-08-01 20:39:41 +0200

    modified mappings

---
+++ cfx_prompt_blaine_county_hall/fxmanifest.lua
@@ -0,0 +1,18 @@
+-- Decrypted & Fixed by Escrow Fucker
+fx_version 'bodacious'
+game 'gta5'
+this_is_a_map 'yes'
+
+data_file 'AUDIO_GAMEDATA' 'audio/prompt_cityhall_game.dat' -- dat151
+data_file 'AUDIO_GAMEDATA' 'audio/prompt_cityhall_doors_game.dat' -- dat151
+
+
+files {
+  'audio/prompt_cityhall_game.dat151.rel',
+  'audio/prompt_cityhall_doors_game.dat151.rel',
+}
+
+escrow_ignore {
+  'stream/unlocked/**'
+}
+dependency '/assetpacks'
